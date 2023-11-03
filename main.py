3832abe46bc23b5ae2dc8060751633e7e82f3e7f
Nicholas J. Primiano <njp2119@columbia.edu>
2023-11-03 16:03:05 -0400

    bug fix

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from torchvision.models.detection import KeypointRCNN
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import albumentations as A
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 keypoints_classes_ids2names = {0: 'Carina' , 1: 'ETT'}
@@ -34,12 +35,6 @@ def get_model(num_keypoints=2, anchor_sizes = (64, 128, 256) , anchor_ratios= (0
         
     return model
 
-
-def load_model(model, weights_path):
-    model_weights = torch.load(weights_path)
-    model.load_state_dict(model_weights['model_state_dict'])
-    return model
-
 def get_best_keypoints(scores):
     avg_scores = []
     for score in scores:
@@ -52,39 +47,64 @@ def get_best_keypoints(scores):
             raise ValueError("avg_scores is empty. Make sure you have calculated the average scores before finding the maximum.")
     except ValueError as e:
         print(e)
-        return []  # or some other default value
-    
-model = load_model(get_model(), "D:\ET_Tube\CheXpert-v1.0\checkpoints\checkpoint__1_5.pt")
+        return []
 
-def process_and_predict(image_file):
-    raw_img = cv2.imread(image_file)
-    aspect_ratio = raw_img.shape[0]/raw_img.shape[1]
+def load_model(model, weights_path):
+    model_weights = torch.load(weights_path)
+    model.load_state_dict(model_weights['model_state_dict'])
+    return model
 
-    if aspect_ratio < 1.5 and aspect_ratio > 0.5:
-        #raw_img = cv2.resize(raw_img, (456, 456))
-        test_img = np.moveaxis(raw_img, -1, 0)
-        test_img= np.expand_dims(test_img, axis=0)
-        test_img = torch.from_numpy(test_img).float().to(device)
+def test_valid_transform():
+    return A.Compose([
+        A.Sequential([
+            A.CLAHE(clip_limit=(1,4), p=1),
+            A.Resize(IMG_SIZE, IMG_SIZE, p=1)
+        ], p=1)
+    ]
+    )
+IMG_SIZE = 456
+def test_valid_transform():
+    return A.Compose([
+        A.Sequential([
+            A.CLAHE(clip_limit=(1,4), p=1),
+            A.Resize(IMG_SIZE, IMG_SIZE, p=1)
+        ], p=1)
+    ]
+    )
 
-        with torch.no_grad():
-            model.to(device)            
-            model.eval()
-            output = model(test_img)
-        
-        scores = output[0]['keypoints_scores']
-        score_idx = get_best_keypoints(scores)
+best_model = load_model(get_model(), "D:\ET_Tube\CheXpert-v1.0\checkpoints\checkpoint__1_5.pt")
 
-        keypoints = output[0]['keypoints'][score_idx].detach().cpu().numpy()
-        for idx, kp in enumerate(keypoints):
+with torch.inference_mode():
+    best_model.to(device)
+    best_model.eval()
+
+    test_img_file = r"C:\Users\nprim\Downloads\A-chest-x-ray-showing-correct-endotracheal-tube-placement-and-no-acute-lung-pathology.png"
+    raw_img = cv2.imread(test_img_file)
+    raw_img_processed = test_valid_transform()(image=raw_img)
+
+    raw_img_processed = raw_img_processed["image"]
+    raw_img_processed = torch.from_numpy(raw_img_processed).permute(2,0,1).float().to(device)
+    raw_img_processed = raw_img_processed.float() / 255.0
+
+    output = best_model([raw_img_processed])
+
+
+    scores = output[0]['scores'].detach().cpu().numpy()
+    high_scores_idxs = np.where(scores > 0.1)[0].tolist() # Indexes of boxes with scores > 0.1
+    post_nms_idxs = torchvision.ops.nms(output[0]['boxes'][high_scores_idxs], output[0]['scores'][high_scores_idxs], 0.3).cpu().numpy() # Indexes of boxes left after applying NMS (iou_threshold=0.3)
+    keypoint_scores = output[0]['keypoints_scores'][post_nms_idxs]
+    score_idx = get_best_keypoints(keypoint_scores)
+
+    keypoints = output[0]['keypoints'][score_idx].detach().cpu().numpy()
+
+    raw_img = cv2.resize(raw_img, (IMG_SIZE, IMG_SIZE))
+    for idx, kp in enumerate(keypoints):
             current_keypoint = kp[:2].astype(int)
-            raw_img = cv2.circle(raw_img, current_keypoint, 1, (255,255,0), 10)
-            image_original = cv2.putText(raw_img, " " + keypoints_classes_ids2names[idx], current_keypoint, cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0), 2, cv2.LINE_AA)
+            print(current_keypoint)
+            raw_img = cv2.circle(raw_img.copy(), current_keypoint, 1, (255,255,0), 10)
 
-        plt.figure(figsize=(10,10))
-        plt.imshow(raw_img)
-        plt.show()
-    else:
-        print(f"Image aspect ratio is {aspect_ratio:0.2F} it but be between 0.5 - 1.5")
+            cv2.putText(raw_img, " " + keypoints_classes_ids2names[idx], current_keypoint, cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0), 1, cv2.LINE_AA)
 
-test_img = r"C:\Users\nprim\Downloads\A-chest-x-ray-showing-correct-endotracheal-tube-placement-and-no-acute-lung-pathology.png"
-process_and_predict(test_img) 
+    plt.figure(figsize=(10,10))
+    plt.imshow(raw_img)
+    plt.show()
