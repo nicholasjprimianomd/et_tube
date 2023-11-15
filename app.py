8a9594e1f10188d9eb54b979594cfa9c712b195f
Nicholas J. Primiano <njp2119@columbia.edu>
2023-11-15 18:02:09 -0500

    visual improvments

--- app.py
+++ app.py
@@ -1,71 +1,3 @@
-"""from flask import Flask, render_template, request, flash, redirect, url_for, send_file
-from werkzeug.utils import secure_filename
-from main import getPrediction
-import os
-
-#Save images to the 'static' folder as Flask serves images from this directory
-UPLOAD_FOLDER = 'static/images/'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-#Create an app object using the Flask class. 
-app = Flask(__name__, static_folder="static")
-
-@app.before_request
-def before_request():
-    if 'X-Forwarded-For' in request.headers:
-        # In a production application, you should have additional
-        # logic to verify that 'X-Forwarded-For' is being sent by your trusted proxy.
-        # You might also want to take only the first IP in the case of a chain of proxies,
-        # or use a more robust solution like Werkzeug's ProxyFix.
-        request.remote_addr = request.headers['X-Forwarded-For']
-
-#Add reference fingerprint. 
-#Cookies travel with a signature that they claim to be legit. 
-#Legitimacy here means that the signature was issued by the owner of the cookie.
-#Others cannot change this cookie as it needs the secret key. 
-#It's used as the key to encrypt the session - which can be stored in a cookie.
-#Cookies should be encrypted if they contain potentially sensitive information.
-app.secret_key = "secret key"
-
-#Define the upload folder to save images uploaded by the user. 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-#Define the route to be home. 
-#The decorator below links the relative route of the URL to the function it is decorating.
-#Here, index function is with '/', our root directory. 
-#Running the app sends us to index.html.
-#Note that render_template means it looks for the file in the templates folder. 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-#Add Post method to the decorator to allow for form submission. 
-@app.route('/', methods=['POST'])
-def submit_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No file selected for uploading')
-            return redirect(request.url)
-        if file:
-            filename = secure_filename(file.filename)  
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-            processed_image_io = getPrediction(filename)
-            processed_image_io.seek(0)  # Ensure to seek back to the start
-            return send_file(processed_image_io, mimetype='image/jpeg')
-        else:
-            flash('Allowed image types are -> png, jpg, jpeg, gif')
-            return redirect(request.url)
-
-
-if __name__ == "__main__":
-    app.run()
-"""
 from flask import Flask, render_template, request, flash, redirect, send_file
 from werkzeug.utils import secure_filename
 from main import getPrediction
@@ -85,26 +17,33 @@ def index():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
+@app.route('/', methods=['POST'])
 def submit_file():
     if 'file' not in request.files:
         flash('No file part')
         return redirect(request.url)
+    
     file = request.files['file']
     if file.filename == '':
         flash('No file selected for uploading')
         return redirect(request.url)
+    
     if file:
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
 
-        # getPrediction should now return an io.BytesIO object
+        # Process the image (your existing processing logic)
         processed_image_io = getPrediction(file_path)
-        
-        # Ensure we're at the start of the BytesIO stream
         processed_image_io.seek(0)
 
-        return send_file(processed_image_io, mimetype='image/jpeg', as_attachment=False)
+        # Send the processed image to the client
+        response = send_file(processed_image_io, mimetype='image/jpeg', as_attachment=False)
+
+        # Delete the original file after sending the processed image
+        os.remove(file_path)
+
+        return response
     else:
         flash('Allowed image types are -> png, jpg, jpeg, gif')
         return redirect(request.url)
